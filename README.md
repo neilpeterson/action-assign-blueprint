35cd806c170998dbccb83a31c7a526a51b7cce7b
Neil Peterson <neilpeterson@outlook.com>
2019-09-11 21:15:44 -0700

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,48 @@
-# action-assign-blueprint
+# GitHub Action: Assign Azure Blueprints
+
+GitHub Actions to assign an Azure Blueprint. For more information on Azure Blueprints, see the [Azure Blueprints documentation](https://docs.microsoft.com/en-us/azure/governance/blueprints/overview?WT.mc_id=blueprintsextension-github-nepeters).
+
+## Azure Authentication
+
+Before using these actions, create an Azure Active Directory Service Principal using the `az ad sp create-for-rbac` command. For more information on creating an Azure Active Directory service principal, see the [Azure Command-Line Interface docs for working with service principals](https://docs.microsoft.com/en-us/cli/azure/ad/sp?WT.mc_id=blueprintsextension-github-nepeters&view=azure-cli-latest).
+
+
+Next, create three GitHub secrets hold the service principal credentials.
+
+
+| Secret Name | Value |
+|:---|---:|
+| AZURETENANTID | tenant |
+| AZURECLIENTID | appId |
+| AZUREPASSWORD | password |
+
+
+## Assign Blueprint
+
+```
+- name: Assign Azure Blueprint
+  env:
+    AZURETENANTID: ${{ secrets.AZURETENANTID }}
+    AZURECLIENTID: ${{ secrets.AZURECLIENTID }}
+    AZUREPASSWORD: ${{ secrets.AZUREPASSWORD }}
+  uses: neilpeterson/action-assign-blueprint@master
+  with:
+    AssignmentName: actionBlueprintPublish
+    azureManagementGroupName: nepeters-internal
+    blueprintName: actionBlueprintPublish
+    azureSubscriptionID: 00000000-0000-0000-0000-000000000000
+    AssignmentFilePath: ./assign/assign-blueprint.json
+```
+
+All configuration parameters:
+
+| Name | Description | Required |
+|:---|:---|---:|
+| azureManagementGroupName | The Azure Management group at which the blueprint is stored. | false |
+| azureSubscriptionID | The Azure subscription at which the blueprint is stored and / or where the blueprint will be assigned. | false |
+| blueprintName | The blueprint name. | true |
+| blueprintLocationScope | < this arg will be removed >   | false |
+| AssignmentFilePath | The path to a JSON file containing the assignment details and parameter values. | true |
+| blueprintVersion | The version of the blueprint to assign. Defaults to `latest`. | false |
+| wait | Wait for assignment to complete before moving to the next task. Defaults to `false`. | false |
+| timeout | Time in seconds before wait timeout'. Defaults to `240 seconds`. | false |
---
+++ entrypoint.ps1
@@ -0,0 +1,81 @@
+<#
+.DESCRIPTION
+    Assign Azure BluePrint
+
+.NOTES
+    Intent: Sample to demonstrate Azure BluePrints with Github Actions
+#>
+
+$ErrorActionPreference = "Stop"
+
+# Auth
+$TenantId = $Env:AZURETENANTID
+$ClientId = $Env:AZURECLIENTID
+$ClientSecret = $Env:AZUREPASSWORD | ConvertTo-SecureString -AsPlainText -Force
+
+# Location Details
+$BlueprintLocation = $Env:INPUT_BLUEPRINTLOCATIONSCOPE
+$BlueprintManagementGroup = $Env:INPUT_AZUREMANAGEMENTGROUPNAME
+$BlueprintSubscriptionID = $Env:INPUT_AZURESUBSCRIPTIONID
+
+$BlueprintName = $Env:INPUT_BLUEPRINTNAME
+$AssignmentName = $Env:INPUT_ASSIGNMENTNAME
+$AssignmentFilePath = $Env:INPUT_ASSIGNMENTFILEPATH
+$Wait = $Env:INPUT_WAIT
+$Timeout = $Env:INPUT_TIMEOUT
+$BlueprintVersion = $Env:INPUT_BLUEPRINTVERSION
+
+# Install Azure PowerShell modules
+Find-Module Az.Accounts | Install-Module -Force
+Find-Module Az.Blueprint | Install-Module -Force
+
+# Set Blueprint Scope (Subscription / Management Group)
+if ($BlueprintLocation -eq 'ManagementGroup') {
+    $BlueprintScope = "-ManagementGroupId $BlueprintManagementGroup"
+}
+
+if ($BlueprintLocation -eq 'Subscription') {
+    $BlueprintScope = "-SubscriptionId $BlueprintSubscriptionID"
+}
+
+# Connect to Azure
+$creds = New-Object System.Management.Automation.PSCredential ($ClientId, $ClientSecret)
+Connect-AzAccount -ServicePrincipal -Tenant $TenantId -Credential creds -WarningAction silentlyContinue
+
+# Get Blueprint object
+if ($BlueprintVersion -eq 'latest') {
+    $bluePrintObject = Invoke-Expression "Get-AzBlueprint -Name $BlueprintName $BlueprintScope"
+ } else {
+    $bluePrintObject = Invoke-Expression "Get-AzBlueprint -Name $BlueprintName $BlueprintScope -Version $BlueprintVersion"
+ }
+
+# Add Blueprint ID
+$body = Get-Content -Raw -Path $AssignmentFilePath | ConvertFrom-Json
+$body.properties.blueprintId = $bluePrintObject.id
+$body | ConvertTo-Json -Depth 4 | Out-File -FilePath $AssignmentFilePath -Encoding utf8 -Force
+
+# Get blueprint assignment
+try {
+    Get-AzBlueprintAssignment -Name $AssignmentName | Out-Null
+    Set-AzBlueprintAssignment -Name $AssignmentName -Blueprint $bluePrintObject -AssignmentFile $AssignmentFilePath -SubscriptionId $BlueprintSubscriptionID
+} catch {
+    New-AzBlueprintAssignment -Name $AssignmentName -Blueprint $bluePrintObject -AssignmentFile $AssignmentFilePath -SubscriptionId $BlueprintSubscriptionID
+}
+
+# Wait for assignment to complete
+if ($Wait -eq "true") {
+    $timeout = new-timespan -Seconds $Timeout
+    $sw = [diagnostics.stopwatch]::StartNew()
+
+    while (($sw.elapsed -lt $timeout) -and ($assignemntStatus.ProvisioningState -ne "Succeeded") -and ($assignemntStatus.ProvisioningState -ne "Failed")) {
+        $assignemntStatus = Get-AzBlueprintAssignment -Name $AssignmentName -SubscriptionId $BlueprintSubscriptionID
+        if ($assignemntStatus.ProvisioningState -eq "failed") {
+            Throw "Assignment Failed. See Azure Portal for datails."
+            break
+        }
+    }
+
+    if ($assignemntStatus.ProvisioningState -ne "Succeeded") {
+        Write-Warning "Assignment has timed out, activity is exiting."
+    }
+}
